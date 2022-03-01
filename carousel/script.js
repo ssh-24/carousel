02cea5cfe9b9d8b21e04b393ea4223d3f778b9b0
Hwan <65848753+ssh-24@users.noreply.github.com>
2022-03-01 12:23:02 +0900

    220301 12:22 캐러셀 구현

--- carousel/script.js
+++ carousel/script.js
@@ -1,89 +1,109 @@
 ;(function () {
-  'use strict'
+    'use strict'
 
-  const get = (target) => {
-    return document.querySelector(target)
-  }
-
-  class Carousel {
-    constructor(caruoselElement) {
-      this.caruoselElement = caruoselElement
-      this.itemClassName = 'carousel_item'
-      this.items = this.caruoselElement.querySelectorAll('.carousel_item')
-      this.totalItems = this.items.length
-      this.current = 0 // 현재 인덱스
+    const get = (target) => {
+        return document.querySelector(target)
     }
 
-    initCarousel() {
-      this.items[this.totalItems - 1].classList.add('prev')
-      this.items[0].classList.add('active')
-      this.items[1].classList.add('next')
-    }
-
-    // 캐러셀 이동
-    moveCarouselTo() {
-      let prev = this.current - 1
-      let next = this.current + 1
-
-      if (this.current === 0) {
-        prev = this.totalItems - 1
-      } else if (this.current === this.totalItems - 1) {
-        next = 0
-      }
-
-      for (let i = 0; i < this.totalItems; i++) {
-        if (i === this.current) {
-          this.items[i].className = this.itemClassName + ' active'
-        } else if (i === prev) {
-          this.items[i].className = this.itemClassName + ' prev'
-        } else if (i === next) {
-          this.items[i].className = this.itemClassName + ' next'
-        } else {
-          this.items[i].className = this.itemClassName
+    class Carousel {
+        constructor(carouselElement) {
+            this.carouselElement = carouselElement
+            this.itemClassName = 'carousel_item';
+            this.items = this.carouselElement.querySelectorAll('.carousel_item') // 유사배열객체
+            
+            this.totalItems = this.items.length; // items의 총 갯수, 5
+            this.current = 0 // 현재 인덱스
         }
-      }
+
+        // 초기설정 - 초기화
+        initCarousel() {
+            this.items[this.totalItems - 1].classList.add('prev')
+            this.items[0].classList.add('active')
+            this.items[1].classList.add('next')
+        }
+
+
+        // 이벤트 등록
+        setEventListener() {
+            this.prevButton = this.carouselElement.querySelector(
+                '.carousel_button--prev'
+            );
+            this.nextButton = this.carouselElement.querySelector(
+                '.carousel_button--next'
+            );
+
+            this.prevButton.addEventListener('click', () => {
+                this.movePrev()
+            })
+            this.nextButton.addEventListener('click', () => {
+                this.moveNext()
+            })
+        }
+
+        
+        moveCarouselTo() {
+            let prev = this.current - 1
+            let next = this.current + 1
+
+            // 끝값일 경우 예외처리
+            if (this.current === 0) {
+                prev = this.totalItems - 1
+            }
+            else if (this.current === this.totalItems - 1) {
+                next = 0
+            }
+
+            // class 추가해서 동작
+            for (let i = 0; i < this.totalItems; i++) {
+                if (i === this.current) {
+                    this.items[i].className = this.itemClassName + ' active' // 띄어쓰기 주의
+                } 
+                else if (i === prev) {
+                    this.items[i].className = this.itemClassName + ' prev'
+                } 
+                else if (i === next) {
+                    this.items[i].className = this.itemClassName + ' next'
+                } 
+                else {
+                    this.items[i].className = this.itemClassName
+                }
+            }
+        }
+
+
+        // 다음
+        moveNext() {
+            if (this.current === this.totalItems - 1) {
+                this.current = 0
+            }
+            else {
+                this.current++
+            }
+            // 동작
+            this.moveCarouselTo()
+        }
+        
+        // 이전
+        movePrev() {
+            if (this.current === 0) {
+                this.current = this.totalItems - 1
+            }
+            else {
+                this.current--
+            }
+            // 동작
+            this.moveCarouselTo()
+        }
+
+
     }
 
-    movePrev() {
-      if (this.current === 0) {
-        this.current = this.totalItems - 1
-      } else {
-        this.current--
-      }
-      this.moveCarouselTo()
-    }
 
-    moveNext() {
-      if (this.current === this.totalItems - 1) {
-        this.current = 0
-      } else {
-        this.current++
-      }
-      this.moveCarouselTo()
-    }
-
-    setEventListeners() {
-      this.prevButton = this.caruoselElement.querySelector(
-        '.carousel_button--prev'
-      )
-      this.nextButton = this.caruoselElement.querySelector(
-        '.carousel_button--next'
-      )
-
-      this.prevButton.addEventListener('click', () => {
-        this.movePrev()
-      })
-      this.nextButton.addEventListener('click', () => {
-        this.moveNext()
-      })
-    }
-  }
-
-  // DOM load 되면 실행
-  document.addEventListener('DOMContentLoaded', () => {
-    const carouselElement = get('.carousel')
-    const carousel = new Carousel(carouselElement)
-    carousel.initCarousel()
-    carousel.setEventListeners()
-  })
+    // DOM Load 시 이벤트 등록
+    document.addEventListener('DOMContentLoaded', () => {
+        const carouselElement = get('.carousel');
+        const carousel = new Carousel(carouselElement); // 인스턴스 생성
+        carousel.initCarousel()
+        carousel.setEventListener()
+    })
 })()
